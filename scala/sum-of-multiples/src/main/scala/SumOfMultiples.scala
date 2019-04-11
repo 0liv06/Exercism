02a0525089148ace7c9058a3b8b196bc6bdf1e03
olivier.donnadei <olivier.donnadei@kaspersky.com>
2019-04-11 10:02:33 +0100

    Completed SumOfMultiples

---
+++ scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -0,0 +1,14 @@
+object SumOfMultiples extends App {
+
+  def isMultiple(a: Int, b: Int): Int = {
+    if (a % b == 0) a
+    else 0
+  }
+
+  def getFactors(factors:Set[Int], limit: Int) = for {
+    a <- 0 until limit
+    b <- factors
+  } yield isMultiple(a, b)
+
+  def sum(factors: Set[Int], limit: Int): Int = getFactors(factors, limit).sum
+}
