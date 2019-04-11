8426ce234b003be7a614ea2eec8dfb7278542e5b
olivier.donnadei <olivier.donnadei@kaspersky.com>
2019-04-11 15:29:50 +0100

    Fifth Iteration of Matrix
    rows and cols are now private and lazy for better performances

--- scala/matrix/src/main/scala/Matrix.scala
+++ scala/matrix/src/main/scala/Matrix.scala
@@ -1,9 +1,8 @@
 case class Matrix(baseMatrix: String) {
 
-  val rows: Vector[Vector[Int]] =  baseMatrix.split("\n").map(f => f.split(" ").map(_.toInt).toVector).toVector
-  val cols: Vector[Vector[Int]] = rows(0).indices.map(idx  => rows.map(_(idx))).toVector
+  lazy private val rows: Vector[Vector[Int]] =  baseMatrix.split("\n").map(f => f.split(" ").map(_.toInt).toVector).toVector
+  lazy private val cols: Vector[Vector[Int]] = rows(0).indices.map(idx  => rows.map(_(idx))).toVector
 
   def row(idx: Int): Vector[Int] = rows(idx)
   def column(idx: Int): Vector[Int] = cols(idx)
-
 }
