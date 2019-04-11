fc7ccf6b3aad7d03ba7ed2d48b8c8ad78c2b9c35
olivier.donnadei <olivier.donnadei@kaspersky.com>
2019-04-11 15:20:14 +0100

    Fourth Iteration of Matrix
    Storing rows and cols so it can be accessed directly from col or row method

--- scala/matrix/src/main/scala/Matrix.scala
+++ scala/matrix/src/main/scala/Matrix.scala
@@ -1,8 +1,9 @@
 case class Matrix(baseMatrix: String) {
 
-  val matrix: Vector[Vector[Int]] =  baseMatrix.split("\n").map(f => f.split(" ").map(_.toInt).toVector).toVector
+  val rows: Vector[Vector[Int]] =  baseMatrix.split("\n").map(f => f.split(" ").map(_.toInt).toVector).toVector
+  val cols: Vector[Vector[Int]] = rows(0).indices.map(idx  => rows.map(_(idx))).toVector
 
-  def row(idx: Int): Vector[Int] = matrix(idx)
-  def column(idx: Int): Vector[Int] = matrix.map(row => row(idx))
+  def row(idx: Int): Vector[Int] = rows(idx)
+  def column(idx: Int): Vector[Int] = cols(idx)
 
 }
