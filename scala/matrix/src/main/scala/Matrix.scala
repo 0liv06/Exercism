c807301d9bf22ab46b030e213f2ed66d7ca8cc62
olivier.donnadei <olivier.donnadei@kaspersky.com>
2019-04-11 11:35:44 +0100

    Completed first iteration of matrix exercise

--- scala/matrix/src/main/scala/Matrix.scala
+++ scala/matrix/src/main/scala/Matrix.scala
@@ -1,6 +1,10 @@
 case class Matrix(matrix: String) {
 
-  def row(idx: Int): Vector[Int] = ???
-  def column(idx: Int): Vector[Int] = ???
 
+  def row(idx: Int): Vector[Int] = matrix.split("\n")(idx).split(" ").map(_.toInt).toVector
+  def column(idx: Int): Vector[Int] = {
+    val mat = matrix.split("\n").map(f => f.split(" ").map(_.toInt))
+    val seq = mat.indices
+    seq.map(f => mat(f)(idx)).toVector
+  }
 }
