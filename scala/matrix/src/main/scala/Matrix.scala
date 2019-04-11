210cc451139e90ef60f0024fc58a0a2d3d1a376a
olivier.donnadei <olivier.donnadei@kaspersky.com>
2019-04-11 14:33:52 +0100

    Third Iteration of Matrix
    Removed companion object and reinstated case class

--- scala/matrix/src/main/scala/Matrix.scala
+++ scala/matrix/src/main/scala/Matrix.scala
@@ -1,13 +1,8 @@
-class Matrix(val baseMatrix: String) {
+case class Matrix(baseMatrix: String) {
 
   val matrix: Vector[Vector[Int]] =  baseMatrix.split("\n").map(f => f.split(" ").map(_.toInt).toVector).toVector
 
   def row(idx: Int): Vector[Int] = matrix(idx)
   def column(idx: Int): Vector[Int] = matrix.map(row => row(idx))
 
-}
-
-object Matrix {
-
-  def apply(baseMatrix: String): Matrix = new Matrix(baseMatrix)
 }
