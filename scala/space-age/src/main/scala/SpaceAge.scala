7feb69b460fabbd21456f840dcb99885b680e506
olivier.donnadei <olivier.donnadei@kaspersky.com>
2019-04-12 14:59:04 +0100

    Second iteration
    - fixed typo

--- scala/space-age/src/main/scala/SpaceAge.scala
+++ scala/space-age/src/main/scala/SpaceAge.scala
@@ -1,12 +1,12 @@
 object SpaceAge {
-  private def gerAgeSeconds(age: Double, ratio: Double): Double = age / (ratio * 31557600)
+  private def getAgeSeconds(age: Double, ratio: Double): Double = age / (ratio * 31557600)
 
-  def onEarth(age: Double): Double = gerAgeSeconds(age, 1)
-  def onMercury(age: Double): Double = gerAgeSeconds(age, 0.2408467)
-  def onVenus(age: Double): Double = gerAgeSeconds(age, 0.61519726 )
-  def onMars(age: Double): Double = gerAgeSeconds(age, 1.8808158)
-  def onJupiter(age: Double): Double = gerAgeSeconds(age, 11.862615)
-  def onSaturn(age: Double): Double = gerAgeSeconds(age, 29.447498)
-  def onUranus(age: Double): Double = gerAgeSeconds(age, 84.016846)
-  def onNeptune(age: Double): Double = gerAgeSeconds(age, 164.79132)
+  def onEarth(age: Double): Double = getAgeSeconds(age, 1)
+  def onMercury(age: Double): Double = getAgeSeconds(age, 0.2408467)
+  def onVenus(age: Double): Double = getAgeSeconds(age, 0.61519726 )
+  def onMars(age: Double): Double = getAgeSeconds(age, 1.8808158)
+  def onJupiter(age: Double): Double = getAgeSeconds(age, 11.862615)
+  def onSaturn(age: Double): Double = getAgeSeconds(age, 29.447498)
+  def onUranus(age: Double): Double = getAgeSeconds(age, 84.016846)
+  def onNeptune(age: Double): Double = getAgeSeconds(age, 164.79132)
 }
