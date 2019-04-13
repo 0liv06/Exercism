caa3826b909f4dab4bc0a0eb3ab9ef3809a6d1d4
0liv06 <odonnadei@gmail.com>
2019-04-13 16:59:04 +0100

    Review Bob to only use Regex in pattern matching

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -1,15 +1,17 @@
-object Bob {
+import scala.util.matching.Regex
 
-  def isMatching(statement: String, pattern: String): Boolean = pattern.r.pattern.matcher(statement.trim).matches()
-  def isShouting(statement: String): Boolean = isMatching(statement, "[^a-z]+[A-Z]+.+")
-  def isAskingQuestion(statement: String): Boolean = isMatching(statement, ".+\\?")
+object Bob {
+  val silence: Regex = "([\\s]*)".r
+  val shouting: Regex = "([^a-z]+[A-Z]+.+)".r
+  val question: Regex = "(.+\\?)\\s*".r
+  val forcefulQuestion: Regex = "([A-Z\\s]+[?])".r
 
   def response(statement: String): String = {
-    (statement.trim.isEmpty, isShouting(statement), isAskingQuestion(statement)) match {
-      case (true, _, _) => "Fine. Be that way!"
-      case (_, true, true) => "Calm down, I know what I'm doing!"
-      case (_, true, false) => "Whoa, chill out!"
-      case (_, false, true) => "Sure."
+    statement match {
+      case silence(_) => "Fine. Be that way!"
+      case forcefulQuestion(_) => "Calm down, I know what I'm doing!"
+      case shouting(_) => "Whoa, chill out!"
+      case question(_) => "Sure."
       case _ => "Whatever."
     }
   }
