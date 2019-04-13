e5802fa3bf987095117150f4c6397c5f14a1bc25
0liv06 <odonnadei@gmail.com>
2019-04-13 18:10:54 +0100

    Completed first iteration of ETL

--- scala/etl/src/main/scala/Etl.scala
+++ scala/etl/src/main/scala/Etl.scala
@@ -1,3 +1,8 @@
+import scala.collection.immutable.TreeMap
+
 object Etl {
-  def transform(map: Map[Int, Seq[String]]): Map[String, Int] = ???
+
+  def transform(values: Map[Int, Seq[String]]): Map[String, Int] = {
+    TreeMap(values.flatMap(f => f._2.map(_.toLowerCase -> f._1)).toSeq:_*)
+  }
 }
