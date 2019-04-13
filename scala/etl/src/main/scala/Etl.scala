f39752fd4d8d9dec1789c263163af21e4bbbcd75
0liv06 <odonnadei@gmail.com>
2019-04-13 18:13:30 +0100

    Second iteration of ETL
    Removed use of TreeMap as sorted key not necessary

--- scala/etl/src/main/scala/Etl.scala
+++ scala/etl/src/main/scala/Etl.scala
@@ -1,8 +1,6 @@
-import scala.collection.immutable.TreeMap
-
 object Etl {
 
   def transform(values: Map[Int, Seq[String]]): Map[String, Int] = {
-    TreeMap(values.flatMap(f => f._2.map(_.toLowerCase -> f._1)).toSeq:_*)
+    values.flatMap(f => f._2.map(_.toLowerCase -> f._1))
   }
 }
