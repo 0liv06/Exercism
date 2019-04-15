8b78156a57f92fa03be5363fdc9ea023f4466b84
olivier.donnadei <olivier.donnadei@kaspersky.com>
2019-04-15 09:36:31 +0100

    First iteration of WordCount

--- scala/word-count/src/main/scala/WordCount.scala
+++ scala/word-count/src/main/scala/WordCount.scala
@@ -1,5 +1,8 @@
 case class WordCount(word: String) {
 
-  def countWords() =  Map()
+  lazy private val wordCount: Array[String] = word.toLowerCase.split("([^A-Za-z0-9'])").map(w => w.trim.stripPrefix("'").stripSuffix("'")).filter(!_.trim.equals(""))
 
+  def countWords(): Map[String, Int] =  {
+    wordCount.map(s => s -> wordCount.count(_.equals(s))).toMap
+  }
 }
