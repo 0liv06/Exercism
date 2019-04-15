6c434a9bc1bfebb911359e51395f06a37e801c5a
olivier.donnadei <olivier.donnadei@kaspersky.com>
2019-04-15 12:04:15 +0100

    third iteration using scala dynamics

--- scala/space-age/src/main/scala/SpaceAge.scala
+++ scala/space-age/src/main/scala/SpaceAge.scala
@@ -1,12 +1,20 @@
-object SpaceAge {
+import scala.language.dynamics
+
+object SpaceAge extends Dynamic {
+
   private def getAgeSeconds(age: Double, ratio: Double): Double = age / (ratio * 31557600)
 
-  def onEarth(age: Double): Double = getAgeSeconds(age, 1)
-  def onMercury(age: Double): Double = getAgeSeconds(age, 0.2408467)
-  def onVenus(age: Double): Double = getAgeSeconds(age, 0.61519726 )
-  def onMars(age: Double): Double = getAgeSeconds(age, 1.8808158)
-  def onJupiter(age: Double): Double = getAgeSeconds(age, 11.862615)
-  def onSaturn(age: Double): Double = getAgeSeconds(age, 29.447498)
-  def onUranus(age: Double): Double = getAgeSeconds(age, 84.016846)
-  def onNeptune(age: Double): Double = getAgeSeconds(age, 164.79132)
+  def applyDynamic(name: String)(args: Double): Double = {
+    name match {
+      case "onEarth" => getAgeSeconds(args, 1)
+      case "onMercury" => getAgeSeconds(args, 0.2408467)
+      case "onVenus" => getAgeSeconds(args, 0.61519726 )
+      case "onMars" => getAgeSeconds(args, 1.8808158)
+      case "onJupiter" => getAgeSeconds(args, 11.862615)
+      case "onSaturn" => getAgeSeconds(args, 29.447498)
+      case "onUranus" => getAgeSeconds(args, 84.016846)
+      case "onNeptune" => getAgeSeconds(args, 164.79132)
+      case _ => 0.0
+    }
+  }
 }
