4e1d43ca09f02a2cf79219a8278088de60e5a4cd
olivier.donnadei <olivier.donnadei@kaspersky.com>
2019-04-16 17:45:19 +0100

    Renamed wordCount variable to words as more representing of variable content

--- scala/word-count/src/main/scala/WordCount.scala
+++ scala/word-count/src/main/scala/WordCount.scala
@@ -1,8 +1,8 @@
 case class WordCount(word: String) {
 
-  lazy private val wordCount: Array[String] = word.toLowerCase.split("([^A-Za-z0-9'])").map(w => w.trim.stripPrefix("'").stripSuffix("'")).filter(!_.trim.equals(""))
+  lazy private val words: Array[String] = word.toLowerCase.split("([^A-Za-z0-9'])").map(w => w.trim.stripPrefix("'").stripSuffix("'")).filter(!_.trim.equals(""))
 
   def countWords(): Map[String, Int] =  {
-    wordCount.map(s => s -> wordCount.count(_.equals(s))).toMap
+    words.map(s => s -> words.count(_.equals(s))).toMap
   }
 }
