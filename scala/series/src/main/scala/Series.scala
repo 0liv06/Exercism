37e77df25b207895678a0a9ac6eb306f286728f9
0liv06 <odonnadei@gmail.com>
2019-04-16 07:45:35 +0100

    Reviewed series, now one line only using inline sequence

--- scala/series/src/main/scala/Series.scala
+++ scala/series/src/main/scala/Series.scala
@@ -1,12 +1,3 @@
 object Series {
-
-  def slices(slice: Int, series: String): List[List[Int]] = {
-    if (series.isEmpty) List()
-    else {
-      val slices = for {
-        s <- 0 to series.length - slice
-      } yield series.substring(s, s + slice).toList.map(f => f.asDigit)
-      slices.toList
-    }
-  }
+  def slices(slice: Int, series: String): List[List[Int]] = (0 to series.length - slice).map(s => series.substring(s, s + slice).toList.map(_.asDigit)).toList
 }
