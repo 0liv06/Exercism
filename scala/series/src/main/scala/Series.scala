c5d9b38de56db6b1048d3fc5b1e451d329e02030
olivier.donnadei <olivier.donnadei@kaspersky.com>
2019-04-16 09:44:23 +0100

    Second iteration of series, simplified range in for comprehension

--- scala/series/src/main/scala/Series.scala
+++ scala/series/src/main/scala/Series.scala
@@ -1,3 +1,3 @@
 object Series {
-  def slices(slice: Int, series: String): List[List[Int]] = (0 to series.length - slice).map(s => series.substring(s, s + slice).toList.map(_.asDigit)).toList
+  def slices(slice: Int, series: String): List[List[Int]] = (0 to series.length - slice).map(i => series.slice(i, i + slice).map(c => c.asDigit).toList).toList
 }
