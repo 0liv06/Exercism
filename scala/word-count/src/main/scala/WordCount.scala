869bc981375c60cafdac98de0899d705a8b63be8
olivier.donnadei <olivier.donnadei@kaspersky.com>
2019-04-17 09:27:53 +0100

    Reviewed implementation to use groupBy instead of count and toMap

--- scala/word-count/src/main/scala/WordCount.scala
+++ scala/word-count/src/main/scala/WordCount.scala
@@ -1,8 +1,8 @@
 case class WordCount(word: String) {
 
   lazy private val words: Array[String] = word.toLowerCase.split("([^A-Za-z0-9'])").map(w => w.trim.stripPrefix("'").stripSuffix("'")).filter(!_.trim.equals(""))
+  lazy private val wordCount: Map[String, Int] = words.groupBy(_.toString).mapValues(_.length)
 
-  def countWords(): Map[String, Int] =  {
-    words.map(s => s -> words.count(_.equals(s))).toMap
-  }
+  def countWords(): Map[String, Int] = wordCount
+
 }
