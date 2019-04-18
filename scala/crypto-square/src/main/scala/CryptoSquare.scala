725f523096102519c74a22bb1ec156c3b1e03b06
olivier.donnadei <olivier.donnadei@kaspersky.com>
2019-04-18 15:24:52 +0100

    Third iteration of crypto square, reviewed code to simplify it

--- scala/crypto-square/src/main/scala/CryptoSquare.scala
+++ scala/crypto-square/src/main/scala/CryptoSquare.scala
@@ -2,26 +2,20 @@ object CryptoSquare {
 
   private def normalize(message: String): String = message.toLowerCase.replaceAll("[^a-z1-9]", "")
 
-  private def numberCols(msgLength: Int, colsIdx: Int): Int = {
-    if (colsIdx >= msgLength / colsIdx) colsIdx
-    else numberCols(msgLength, colsIdx + 1)
+  private def numberCols(msgLength: Int): Int = {
+    Math.sqrt(msgLength).toInt
   }
 
   def ciphertext(message: String): String = {
     if (message.isEmpty) ""
     else {
-      val normalizedString = normalize(message)
-      val slideIndex = numberCols(normalizedString.length, 1)
-      val numCols = if (slideIndex % normalizedString.length > 5) slideIndex + 1 else slideIndex
-      val chunks: List[String] = normalizedString.sliding(numCols, numCols).toList
-      val encodedMessage = for {
-        index <- 0 until chunks.head.length
-        chunkIndex <- chunks.indices
-      } yield {
-        if (index < chunks(chunkIndex).length ) chunks(chunkIndex)(index)
-        else " "
-      }
-      encodedMessage.sliding(slideIndex, slideIndex).map(f => f.mkString).mkString(" ")
+      val normalizedMessage = normalize(message)
+      val numRows = numberCols(normalizedMessage.length)
+      val numCols = if ((numRows * numRows) % normalizedMessage.length > 0) numRows + 1 else numRows
+      val chunks: List[String] = normalizedMessage.sliding(numCols, numCols).toList.map(f => f.padTo(numCols, " ").mkString)
+      val encodedMessage = (0 until numCols).map(idx => chunks.map(_(idx))).toList
+
+      encodedMessage.map(_.mkString).mkString(" ")
     }
   }
 }
