b7740126dbeee23f902abf15533add47701bc263
olivier.donnadei <olivier.donnadei@kaspersky.com>
2019-04-18 16:09:07 +0100

    Renamed method getColumns to getRows more meaningful

--- scala/crypto-square/src/main/scala/CryptoSquare.scala
+++ scala/crypto-square/src/main/scala/CryptoSquare.scala
@@ -2,7 +2,7 @@ object CryptoSquare {
 
   private def normalize(message: String): String = message.toLowerCase.replaceAll("[^a-z1-9]", "")
 
-  private def numberCols(msgLength: Int): Int = {
+  private def numberRows(msgLength: Int): Int = {
     Math.sqrt(msgLength).toInt
   }
 
@@ -10,7 +10,7 @@ object CryptoSquare {
     if (message.isEmpty) ""
     else {
       val normalizedMessage = normalize(message)
-      val numRows = numberCols(normalizedMessage.length)
+      val numRows = numberRows(normalizedMessage.length)
       val numCols = if ((numRows * numRows) % normalizedMessage.length > 0) numRows + 1 else numRows
       val chunks: List[String] = normalizedMessage.sliding(numCols, numCols).toList.map(f => f.padTo(numCols, " ").mkString)
       val encodedMessage = (0 until numCols).map(idx => chunks.map(_(idx))).toList
