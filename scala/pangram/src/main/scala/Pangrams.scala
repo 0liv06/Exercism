b50e770d82112190f8c4bfd9631f289956b63d20
olivier.donnadei <olivier.donnadei@kaspersky.com>
2019-04-29 15:14:59 +0100

    Second iteration of Pangram exercise - All tests passing

--- scala/pangram/src/main/scala/Pangrams.scala
+++ scala/pangram/src/main/scala/Pangrams.scala
@@ -1,4 +1,4 @@
 object Pangrams {
-  def isPangram(input: String): Boolean = input.toLowerCase.replaceAll("[^a-zA-Z]", "").toCharArray.toSet.size >= 26
+  def isPangram(input: String): Boolean = input.toLowerCase.filter(_.isLetter).distinct.length >= 26
 }
 
