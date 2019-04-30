45071f420d0681ffb2194ba3307eee26ce8f5d48
olivier.donnadei <olivier.donnadei@kaspersky.com>
2019-04-30 09:32:59 +0100

    Second iteration of MatchingBrackets - all tests passing

--- scala/matching-brackets/src/main/scala/MatchingBrackets.scala
+++ scala/matching-brackets/src/main/scala/MatchingBrackets.scala
@@ -1,19 +1,16 @@
+import scala.annotation.tailrec
+
 object MatchingBrackets {
 
   val matches = Map('{' -> '}', '[' -> ']', '(' -> ')')
 
   def isPaired(s: String): Boolean = {
 
+    @tailrec
     def hasPair(candidate: String, index: Int, result: Boolean): Boolean = {
       if (!result || index == 0) result
-      else {
-        val a = matches.get(candidate.head)
-        val b = Option(candidate(candidate.length -1))
-        val c = candidate.tail.headOption
-
-        if (a.equals(c)) hasPair(candidate.tail.tail, index -1, a.equals(c))
-        else hasPair(candidate.tail.dropRight(1), index - 1, a.equals(b))
-      }
+      else if (matches.get(candidate.head).equals(candidate.tail.headOption)) hasPair(candidate.tail.tail, index -1, matches.get(candidate.head).equals(candidate.tail.headOption))
+      else hasPair(candidate.tail.dropRight(1), index - 1, matches.get(candidate.head).equals(Option(candidate(candidate.length -1))))
     }
 
     val filtered = s.toLowerCase.replaceAll("[^\\[\\]\\{\\}\\(\\)]", "")
